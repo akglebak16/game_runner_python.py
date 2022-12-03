b37f261f77dddd3636508d3d8c8af3750ac50751
akglebak16 <akglebak16@gmail.com>
2022-12-03 01:43:44 +0100

    sss

--- igra_suka.py
+++ igra_suka.py
@@ -5,17 +5,39 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Flexer")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+text_surface = test_font.render('Flexer the game', False, 'black')
+score_rect = text_surface.get_rect(center=(400, 50))
 
+snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_rectangle = snail_surface.get_rect(bottomright=(700, 300))
+
+player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rectangle = player_surface.get_rect(midbottom=(80, 300))
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rectangle.collidepoint(event.pos):
+        #         print("collisions11")
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (score_rect))
+    snail_rectangle.x -= 4
+    if snail_rectangle.right <= 0: snail_rectangle.left = 800
+    screen.blit(snail_surface, snail_rectangle)
+    screen.blit(player_surface, player_rectangle)
 
-    screen.blit(sky_surface,(0, 0))
-    screen.blit(ground_surface,(0, 300))
+    # # if player_rectangle.colliderect(snail_rectangle):
+    # #     print("col")
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rectangle.collidepoint(mouse_pos):
+    #     print("collision")
+
     pygame.display.update()
     clock.tick(60)
